ec4f101f04cdbd72c1925f7972e12e8f0598ce7a
henriquepablo <pablo269704@gmail.com>
2024-07-07 13:28:18 -0300

    feat: adding logo and text description

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { GluestackUIProvider, StatusBar} from "@gluestack-ui/themed";
 import Login from "./Login";
+import { config } from "@gluestack-ui/config";
 
 const App = (): React.JSX.Element => {
   return( 
-    <GluestackUIProvider>
+    <GluestackUIProvider config={config}>
       <StatusBar backgroundColor="#000" />
       <Login />
     </GluestackUIProvider>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { LinearGradient, SafeAreaView, Text } from "@gluestack-ui/themed";
+import { Center, Image, SafeAreaView, Text } from "@gluestack-ui/themed";
 import {LinearGradient as Gradient} from 'react-native-linear-gradient';
 
 const Login = (): React.JSX.Element => {
   return(
     <SafeAreaView flex={1}> 
          <Gradient colors={["#000000c1","#262626", "#090909f5"]} style={{flex: 1}} start={{x: 0, y: 0}} end={{x: 0, y: 0.2}} >
+            <Center>
+                <Image source={require('./spotify.png')} alt="logo do spotify" mt={161} />
+                
+                <Text fontWeight="$semibold" color="#fff" mt={96} fontSize={17}>
+                    Milhões de músicas à sua escolha.
+                </Text>
+                <Text color="#fff" fontWeight="$semibold" fontSize={17}>
+                    Grátis no Spotify.
+                </Text>
             
+            </Center>
          </Gradient>
     </SafeAreaView>
   );
